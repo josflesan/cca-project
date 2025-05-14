6ff3cc150b8d7e164e4ffa617a88d19913419bd7
Josue Fleitas <josue.fle.sanc@gmail.com>
2025-05-14 14:18:09 +0200

    Goated Sub-3 Minute Part 3 Schedule
    Co-authored-by: prest <prestance1@users.noreply.github.com>

--- scripts/part3_schedule.py
+++ scripts/part3_schedule.py
@@ -7,6 +7,38 @@ import argparse
 from typing import List
 
 from utils.part3_utils import get_time
+# 1st shot
+# {
+#     "memcached": "node-b",
+#     "blackscholes": "node-b",
+
+#     "ferret": "node-c", #high flops
+#     "radix": "node-c", #wokring set fits in it, maybe run on 2 cores 4 threads or somehting like that
+    
+#     "freqmine": "node-d", #isolated
+
+#     "vips": "node-a"
+#     "dedup": "node-a",
+#     "canneal": "node-a"
+# }
+
+
+# 2nd shot, vips has a lot of speedup of running on 4 cores
+# {
+#     "memcached": "node-b",
+#     "blackscholes": "node-b",
+
+#     "ferret": "node-c", #high flops
+#     "radix": "node-c", #wokring set fits in it, maybe run on 2 cores 4 threads or somehting like that
+    
+#     "freqmine": "node-d", #isolated
+#     "vips": "node-d", 
+
+#     "dedup": "node-a",
+#     "canneal": "node-a"
+# }
+
+
 
 @dataclasses.dataclass
 class Job:
@@ -15,6 +47,7 @@ class Job:
     node: str
     isolated: bool = False
     thread_num: int = 1
+    coresToPin: int | None = None
     coresReq: int | None = None
     memoryReq: int | None = None
     coresLim: int | None = None
@@ -25,38 +58,42 @@ class Job:
 
 jobs = [
     Job(
-        name="dedup",
-        priority=1,
-        isolated=False,
-        thread_num=2,
-        node="node-a-2core",
-        # coresLim=1,
-        # memoryLim=8,
-    ),
-    Job(
         name="blackscholes",
         priority=1,
-        isolated=True,
-        thread_num=2,
+        isolated=False,
+        thread_num=1,
         node="node-b-2core",
+        coresToPin=1,
         # coresLim=1,
+        # coresReq=1,
         # memoryLim=1,
     ),
     Job(
         name="ferret",
-        priority=2,
-        isolated=True,
+        priority=1,
+        isolated=False,
         thread_num=4,
-        node="node-d-4core",
+        node="node-c-4core",
         # coresReq=3,
         # memoryReq=14,
+    ),
+    Job(
+        name="canneal",
+        priority=1,
+        isolated=False,
+        thread_num=2,
+        node="node-c-4core",
+        # coresReq=2,
+        # memoryReq=8,
+        # coresLim=2,
+        # memoryLim=8,
     ),
     Job(
         name="freqmine",
         priority=1,
         isolated=True,
         thread_num=4,
-        node="node-c-4core",
+        node="node-d-4core",
         # coresReq=4,
         # memoryReq=8,
         # coresLim=4,
@@ -64,31 +101,29 @@ jobs = [
     ),
     Job(
         name="radix",
-        priority=1,
-        isolated=True,
-        thread_num=4,
+        priority=2,
+        isolated=False,
+        thread_num=8,
         node="node-d-4core",
         # coresLim=1,
         # memoryLim=2,
     ),
     Job(
         name="vips",
-        priority=2,
-        isolated=True,
-        thread_num=2,
-        node="node-b-2core",
-        # coresReq=2,
-        # memoryReq=2,
-    ),
-    Job(
-        name="canneal",
-        priority=2,
+        priority=1,
         isolated=False,
         thread_num=2,
         node="node-a-2core",
         # coresReq=2,
-        # memoryReq=8,
-        # coresLim=2,
+        # memoryReq=2,
+    ),
+    Job(
+        name="dedup",
+        priority=2,
+        isolated=False,
+        thread_num=2,
+        node="node-a-2core",
+        # coresLim=1,
         # memoryLim=8,
     ),
 ]
@@ -99,6 +134,10 @@ def wait_for_job_completion(job_name: str, timeout_seconds: int = 3600):
     result = subprocess.run(wait_cmd, shell=True, text=False)
     return result.returncode == 0
 
+def set_cpu_resources(container, cpu_value):
+    container.setdefault("resources", {})
+    container["resources"].setdefault("requests", {})["cpu"] = str(cpu_value)
+    container["resources"].setdefault("limits", {})["cpu"] = str(cpu_value)
 
 def schedule_jobs(jobs: List[Job]):
     jobs = sorted(jobs, key=lambda j: j.priority)
@@ -106,10 +145,12 @@ def schedule_jobs(jobs: List[Job]):
         start_job_cmd = f"kubectl create -f {job.path_name}"
         print(f"starting {job.name}...")
         _ = subprocess.run(start_job_cmd, text=False, shell=True)
+
         if job.isolated:
             print(f"waiting for {job.name}...")
             wait_for_job_completion(job.name)
             print(f"done waiting for {job.name}...")
+
     # not sure about this
     for job in jobs:
         if not job.isolated:
@@ -121,22 +162,32 @@ def update_yaml(job):
         data = yaml.safe_load(file)
         spec = data["spec"]["template"]["spec"]
 
+        # Get the container and add resources key if needed
+        container = spec["containers"][0]
+
+        if job.coresReq or job.memoryReq or job.coresLim or job.memoryLim:
+            container.setdefault("resources", {})
+        elif "resources" in container:
+            container.pop("resources")
+        
         # Change resources
         if job.coresReq:
-            spec["containers"][0]["resources"]["requests"]["cpu"] = str(job.coresReq)
+            container["resources"].setdefault("requests", {})["cpu"] = str(job.coresReq)
         if job.memoryReq:
-            spec["containers"][0]["resources"]["requests"]["memory"] = f"{job.memoryReq}Gi"
+            container["resources"].setdefault("requests", {})["memory"] = f"{job.memoryReq}Gi"
         if job.coresLim:
-            spec["containers"][0]["resources"]["limits"]["cpu"] = str(job.coresLim)
+            container["resources"].setdefault("limits", {})["cpu"] = str(job.coresLim)
         if job.memoryLim:
-            spec["containers"][0]["resources"]["limits"]["memory"] = f"{job.memoryLim}Gi"
+            container["resources"].setdefault("limits", {})["memory"] = f"{job.memoryLim}Gi"
 
         # change node
         spec["nodeSelector"]["cca-project-nodetype"] = job.node
 
         # change thread count
         library = "parsec" if job.name != "radix" else "splash2x"
-        run_cmd = f"./run -a run -S {library} -p {job.name} -i native -n {job.thread_num}"
+        
+        run_cmd_prefix = f"taskset -c '{job.coresToPin}'" if job.coresToPin else ""
+        run_cmd =  run_cmd_prefix + f" ./run -a run -S {library} -p {job.name} -i native -n {job.thread_num}"
         spec["containers"][0]["args"][1] = run_cmd
 
     # write back
@@ -156,12 +207,11 @@ def main(run: int):
     # Run commands to get final time:
     # $ kubectl get pods -o json > results.json
     # $ python3 get_time.py results.json
-    subprocess.run(f"kubectl get pods -o json > logs/pods_{run}.json", shell=True)
-    get_time(f"logs/pods_{run}.json")
+    subprocess.run(f"kubectl get pods -o json > logs/part3/pods_{run}.json", shell=True)
+    get_time(f"logs/part3/pods_{run}.json")
 
     # Cleanup
-    subprocess.run("kubectl delete jobs --all", shell=True)
-    subprocess.run("kubectl delete pods --all", shell=True)
+    subprocess.run("kubectl delete jobs --all", shell=True)    
 
 
 if __name__ == "__main__":
